a9bd8fba4322e32cfbd0b4896b835d5be00b74ba
JHud <219741840+JamHudson@users.noreply.github.com>
2026-02-24 14:15:39 -0800

    Set a temporary page to view billing

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 
 const gridElements= {imgString: "/images/mbLogo.png", amount:20};
-// defualt route
-app.get('/', (req,res) => {
 
+// Default route
+app.get('/', (req,res) => {
   res.render(`home`, {gridElements});
-})
+});
+
+app.get('/billing', (req,res) => {
+  res.sendFile(`${import.meta.dirname}/views/billing.html`)
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port: http://localhost:${PORT}`)
