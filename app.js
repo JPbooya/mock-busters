3f5495c08ce89c26f264e70b7e049f0b9b107cc7
JHud <219741840+JamHudson@users.noreply.github.com>
2026-03-04 02:42:57 -0800

    Admin page

--- app.js
+++ app.js
@@ -10,28 +10,28 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 
-const gridElements= {imgString: "/images/mbLogo.png", amount:20};
+const gridElements = { imgString: "/images/mbLogo.png", amount: 20 };
 
 const orders = []
 
 // Default route
-app.get('/', (req,res) => {
-  res.render(`home`, {gridElements});
+app.get('/', (req, res) => {
+  res.render(`home`, { gridElements });
 });
 
 
-app.get('/billing', (req,res) => {
+app.get('/billing', (req, res) => {
   res.render(`billing`);
 });
-app.get('/movie-info', (req,res) => {
+app.get('/movie-info', (req, res) => {
   res.render(`movieInfo`);
 });
 
-app.get('/admin', (req,res) => {
-  res.send(orders);
+app.get('/admin', (req, res) => {
+  res.render(`admin`, { orders });
 });
 
-app.post('/submit',(req,res) => {
+app.post('/submit', (req, res) => {
   const order = {
     firstName: req.body.fname,
     lastName: req.body.lname,
@@ -39,15 +39,16 @@ app.post('/submit',(req,res) => {
     city: req.body.cname,
     state: req.body.sname,
     zipcode: req.body.zname,
-    creditcard: req.body["credit-card-name"],
-    expiration: req.body.exname,
-    extension: req.body["expiration-name"],
-    delivery: req.body.delivery,
+    creditcard: req.body["credit-name"],
+    extension: req.body["extension-name"],
+    expiration: req.body["exname"],
+    delivery: req.body["dtd-name"],
     timestamp: new Date()
   }
-  res.render(`confirmation`, {
-    order
-  })
+
+  orders.push(order);
+
+  res.render(`confirmation`, { order });
 });
 
 app.listen(PORT, () => {
