846d9d650236799e9ef538ef3cc8b9f0a592698a
JPbooya <perezjcarlos81@gmail.com>
2026-03-16 18:02:19 -0700

    added validation file

---
+++ validation.js
@@ -0,0 +1,49 @@
+// Validates form data on the server side before processing.
+export function validateForm(data) {
+  console.log("Server side validation happens here");
+  console.log(data);
+
+
+const errors = [];
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Validate first name
+  if (data.fname.trim() == "") {
+    errors.push("First name is required.");
+    
+  }
+
+  console.log(errors);
+
+  // Validates last name 
+  if (data.lname.trim() == "") {
+    errors.push("Last name is required.");
+  }
+
+   // Validates email 
+  if (data.email.trim() == "" || !emailRegex.test(data.email)) {
+    errors.push("Email is required.");
+  }
+
+   // Validates city name 
+  if (data.cname.trim() == "") {
+    errors.push("City is required.");
+  }
+
+   // Validates last name 
+  if (data.sname.trim() == "") {
+    errors.push("State is required.");
+  }
+
+   // Validates last name 
+  if (data.zname.trim() == "" || data.zname.length != 5) {
+    errors.push("Zip code is required.");
+  }
+
+
+  return {
+    isValid: errors.length === 0,
+    errors
+  };
+}
+
